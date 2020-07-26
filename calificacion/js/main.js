819359467c573ac800767ade84df3c44fc9c60a6
Santiago Aguilar H <santiago.aguilar.821@gmail.com>
2020-07-25 23:24:13 -0500

    + Se ajustan estilos en las páginas de restaurantes + Se ajusta js para cerrar el restaurante y volver al inicio

--- calificacion/js/main.js
+++ calificacion/js/main.js
@@ -1,39 +1,14 @@
-//console.log : imprime una cadena de texto
+const cerrar = document.querySelector('.cerrar');
 
-//  DOM se representa  document
-//querySelector : devuelve un elemento  aunque existan muchos elementos que cumplan el criterio de busqueda
-//querySekectorAll : Busca en todo el arbol buscando todos los elementos que cumplan con el citerio de busqueda
-/*let links = document.querySelectorAll("a");
-links.forEach(function(link){
-    console.log(link);
-})
-*/
+cerrar.addEventListener('click', function (ev) {
+  ev.preventDefault();
 
-//obtener los elementos de la clase cerrar
-let cerrar = document.querySelectorAll(".cerrar");
+  const content = document.querySelector('.container__content');
 
-//recorrerlos
-cerrar.forEach(function (cerrar) {
-  //Agregar un evento click a cada uno de ellos
-  cerrar.addEventListener("click", function (ev) {
-    ev.preventDefault();
+  content.classList.remove('animate__fadeInDown');
+  content.classList.add('animate__fadeOutUp');
 
-    let content = document.querySelector(".content");
-    // Quitar las clases de animacion que ya tiene
-    content.classList.remove("animate__fadeInDown");
-    content.classList.remove("animate__animated");
-
-    //Agregar clases para animar su salida fadeOutUp
-    content.classList.add("animate__fadeOutUp");
-    content.classList.add("animate__animated");
-
-    //setTimeout / setInterval    Es pera decidir cual empieza primero
-
-  
-    setTimeout(function () {
-      location.href = "/calificacion";
-    }, 600);
-
-    return false;
-  });
+  setTimeout(function () {
+    location.href = '../index.html';
+  }, 600);
 });
